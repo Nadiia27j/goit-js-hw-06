158a028e12218ce9edfc35b0c92761d6a07a8002
Надія Ткач <nadiya77723@gmail.com>
2022-08-16 21:10:24 +0300

    редагує task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,8 +2,39 @@
 const catEl = document.querySelectorAll('.item');
 console.log('Number of categories:', catEl.length);
 
-catEl.forEach(function (categories) {
+catEl.forEach(categories => {
+
     console.log("Category:", categories.firstElementChild.textContent);
-    console.log("Elements:", categories.lastElementsChild.length);
-})
+    console.log("Elements:", categories.lastElementChild.children.length);
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
