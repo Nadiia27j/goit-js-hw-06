320a1493bce50f34ca34909002e83c9a79ad2dfd
Надія Ткач <nadiya77723@gmail.com>
2022-08-16 16:19:46 +0300

    add task 01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+
+const catEl = document.querySelectorAll('.item');
+console.log('Number of categories:', catEl.length);
+
+catEl.forEach(function (categories) {
+    console.log("Category:", categories.firstElementChild.textContent);
+    console.log("Elements:", categories.lastElementsChild.length);
+})
+
+console.log(catEl)
