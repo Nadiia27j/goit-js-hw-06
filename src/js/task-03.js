c56c44265f5f8e33dc521bf2e631458c8c2d04bc
Надія Ткач <nadiya77723@gmail.com>
2022-08-16 22:28:04 +0300

    add task 3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,23 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+const itemEl = document.createElement('li');
+itemEl.classList.add('gallery__item');
+
+const imageEl = document.createElement('img');
+imageEl.classList.add('gallery__image');
+imageEl.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+imageEl.alt = 'White and Black Long Fur Cat';
+imageEl.width = 320;
+imageEl.height = 320;
+
+
+
+
+galleryEl.append(itemEl, imageEl);
+itemEl.append(imageEl);
