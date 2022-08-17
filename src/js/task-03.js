0c0fc65670317e3672a021d0d71b1ff466cef2a6
Надія Ткач <nadiya77723@gmail.com>
2022-08-17 16:41:23 +0300

    decides task 3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -14,22 +14,14 @@ const images = [
 ];
 
 
+
 const galleryEl = document.querySelector('.gallery');
+// galleryEl.style.display = "flex";
 console.log(galleryEl);
+ 
 
 
-const itemEl = document.createElement('li');
-itemEl.classList.add('gallery__item');
+const elements = images.map((image) => `<li class="gallery__item"><img class"gallery__img" src="${image.url} alt="${image.alt}" width="320" height="320"></li>`);
 
-const imageEl = document.createElement('img');
-imageEl.classList.add('gallery__image');
-imageEl.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-imageEl.alt = 'White and Black Long Fur Cat';
-imageEl.width = 320;
-imageEl.height = 320;
+galleryEl.insertAdjacentHTML("afterbegin", elements.join(""));
 
-
-
-
-galleryEl.append(itemEl, imageEl);
-itemEl.append(imageEl);
