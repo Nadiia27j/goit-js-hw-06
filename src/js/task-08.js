53605c4fcdac2e7ad4bbdd3001b2e974874352fe
Надія Ткач <nadiya77723@gmail.com>
2022-08-19 20:46:09 +0300

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,39 @@
 
+const form = document.querySelector('.login-form');
+console.log(form);
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+
+  
+  formData.forEach((value, name) => {
+    console.log('name', name);
+    console.log('value', value);
+  });
+}
+
+
+
+
+
+
+
+// const formElements = event.currentTarget.elements;
+
+//   console.dir(formElements);
+
+//   const mail =  formElements.email.value;
+//   const password = formElements.password.value;
+
+//   console.log(mail, password);
+
+//   const formData = {
+//     mail,
+//     password,
+//   };
+
+//   console.log(formData);
